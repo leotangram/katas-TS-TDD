83409753056387000917047ca73c194e15566d96
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-02-28 12:09:11 -0500

    red(csvFilter): should exclude lines with both tax fields populated as they are exclusive

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -9,4 +9,13 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header, invoiceLine]);
 	});
+
+	test('should exclude lines with both tax fields populated as they are exclusive', () => {
+		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+		const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
