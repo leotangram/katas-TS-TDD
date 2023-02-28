e60619cf7af42e8ee4d87f90ef3b94d8215e8acc
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-02-28 12:00:32 -0500

    red(csvFilter): allows for correct lines only

---
+++ src/tests/csvFilter.test.ts
@@ -0,0 +1,12 @@
+import { CsvFilter } from '../core/csvFilter';
+
+describe('CSV Filter', () => {
+	test('should allows for correct lines only', () => {
+		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+		const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+		const csvFilter = new CsvFilter([header, invoiceLine]);
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header, invoiceLine]);
+	});
+});
