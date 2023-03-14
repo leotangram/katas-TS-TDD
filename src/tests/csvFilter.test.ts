0080265640379bcd7d645704ae438efc7419981e
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-03-14 12:10:06 -0500

    red: should excludes lines with tax fields populated even if non decimal

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -37,4 +37,13 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	test('should excludes lines with tax fields populated even if non decimal', () => {
+		const invoiceLine = '1,02/05/2021,1000,790,XYZ,7,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
