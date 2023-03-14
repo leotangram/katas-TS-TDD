0de38770d1207c87a6f405c804d1dcf7268833ed
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-03-14 12:04:19 -0500

    refactor: Introduce multiple variable. Move variable

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -1,8 +1,9 @@
 import { CsvFilter } from '../core/csvFilter';
 
 describe('CSV Filter', () => {
+	const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+
 	test('should allows for correct lines only', () => {
-		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
 		const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 		const result = csvFilter.filteredLines;
@@ -11,7 +12,6 @@ describe('CSV Filter', () => {
 	});
 
 	test('should exclude lines with both tax fields populated as they are exclusive', () => {
-		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
 		const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
@@ -21,7 +21,6 @@ describe('CSV Filter', () => {
 	});
 
 	test('should excludes lines with both tax field empty as one is required', () => {
-		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
 		const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
