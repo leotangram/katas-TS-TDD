e9589ba242f0336ad29b5ce09225dbf78b05afcd
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-03-14 11:58:56 -0500

    red: should excludes lines with both tax field empty as one is required

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -19,4 +19,14 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	test('should excludes lines with both tax field empty as one is required', () => {
+		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+		const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
