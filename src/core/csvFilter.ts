532897f26d7c552e017e6908923ae8c561448913
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-03-21 11:16:43 -0500

    refactor: extract method

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -9,25 +9,27 @@ export class CsvFilter {
 		const header = this.lines[0];
 		const invoices = this.lines.slice(1);
 
-		return [header].concat(
-			invoices.filter((invoice) => {
-				const fields = invoice.split(',');
-				const ivaField = fields[4];
-				const igicField = fields[5];
-				const decimalRegex = '\\d+(\\.\\d+)?';
-				const taxFieldsAreMutuallyExclusive =
-					(ivaField.match(decimalRegex) || igicField.match(decimalRegex)) && (!ivaField || !igicField);
-				const grossAmountField = fields[2];
-				const netAmountField = fields[3];
-				const netAmountIsWellCalculated =
-					this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, ivaField) ||
-					this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, igicField);
-				const identifyFieldsAreMutuallyExclusive = !fields[7] || !fields[8];
-
-				return taxFieldsAreMutuallyExclusive && netAmountIsWellCalculated && identifyFieldsAreMutuallyExclusive;
-			})
-		);
+		return [header].concat(invoices.filter(this.validateInvoice()));
 	}
+
+	private validateInvoice = () => {
+		return (invoice) => {
+			const fields = invoice.split(',');
+			const ivaField = fields[4];
+			const igicField = fields[5];
+			const decimalRegex = '\\d+(\\.\\d+)?';
+			const taxFieldsAreMutuallyExclusive =
+				(ivaField.match(decimalRegex) || igicField.match(decimalRegex)) && (!ivaField || !igicField);
+			const grossAmountField = fields[2];
+			const netAmountField = fields[3];
+			const netAmountIsWellCalculated =
+				this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, ivaField) ||
+				this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, igicField);
+			const identifyFieldsAreMutuallyExclusive = !fields[7] || !fields[8];
+
+			return taxFieldsAreMutuallyExclusive && netAmountIsWellCalculated && identifyFieldsAreMutuallyExclusive;
+		};
+	};
 
 	private checkIfNetAmountIsCorrect(netAmountField: string, grossAmountField: string, taxField: string) {
 		const parsedNetAmount = parseFloat(netAmountField);
