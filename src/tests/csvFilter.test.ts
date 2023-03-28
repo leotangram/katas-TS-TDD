394f85d5d7297902f8b3c8473ba8687c42cde769
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-03-28 09:49:46 -0500

    red: should does not allow a list of invoices with a single line

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -120,6 +120,13 @@ describe('CSV Filter', () => {
 		expect(result).toEqual([]);
 	});
 
+	test('should does not allow a list of invoices with a single line', () => {
+		const invoiceLine = fileWithOneInvoiceLineHaving({});
+		const result = () => CsvFilter.create([invoiceLine]);
+
+		expect(result).toThrow();
+	});
+
 	function fileWithOneInvoiceLineHaving({
 		invoiceId = '1',
 		ivaTax = '21',
