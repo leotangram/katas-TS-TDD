c6cb22b82be59cf7e41a9079dbb94882ed72054c
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-04 16:28:14 -0500

    red(fibonacci): should yields value one to number one

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -4,4 +4,8 @@ describe('The fibonacci sequence', () => {
 	test('should yields value zero to number zero', () => {
 		expect(fibonacci(0)).toBe(0);
 	});
+
+	test('should yields value one to number one', () => {
+		expect(fibonacci(1)).toBe(1);
+	});
 });
