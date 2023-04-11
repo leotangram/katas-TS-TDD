af89b2e2d88f42538884330b5ca2c2ccc4e4b48d
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-11 11:29:24 -0500

    red(primeFactors): should finds the prime composition of the given number

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -18,5 +18,6 @@ describe('The prime factors', () => {
 		expect(getPrimeFactorsFor(3)).toEqual([3]);
 		expect(getPrimeFactorsFor(3 * 3)).toEqual([3, 3]);
 		expect(getPrimeFactorsFor(2 * 3)).toEqual([2, 3]);
+		expect(getPrimeFactorsFor(5 * 5)).toEqual([5, 5]);
 	});
 });
