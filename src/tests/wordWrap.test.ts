e974fde2c526472de5e1f8d1cd4bff6c4b9f3a22
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-18 15:49:51 -0500

    red(wordWrap): should makes every single line of text fit column width

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -4,5 +4,6 @@ describe('The Word Wrap', () => {
 	test('should makes every single line of text fit column width', () => {
 		expect(wordWrap('hello', 5)).toBe('hello');
 		expect(wordWrap('longword', 4)).toBe('long\nword');
+		expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
 	});
 });
