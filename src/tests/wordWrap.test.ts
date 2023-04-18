91641b3248b1a279823216c1d9667190f407c1f6
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-18 15:39:20 -0500

    red(wordWrap): should makes every single line of text fit column width

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -3,5 +3,6 @@ import { wordWrap } from '../core/wordWrap';
 describe('The Word Wrap', () => {
 	test('should makes every single line of text fit column width', () => {
 		expect(wordWrap('hello')).toBe('hello');
+		expect(wordWrap('longword')).toBe('long\nword');
 	});
 });
