ad9d1f5cbf534a6bdbd57f0bb057e7ce26353b0f
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-18 15:46:00 -0500

    refactor(wordWrap): Flip condition, introduce explanatory variable

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,6 +1,7 @@
 export function wordWrap(text: string, columnWidth: number): string {
-	if (text.length > columnWidth) {
-		return `${text.substring(0, columnWidth)}\n${text.substring(columnWidth)}`;
-	}
-	return text;
+	if (text.length <= columnWidth) return text;
+
+	const wrappedText = `${text.substring(0, columnWidth)}\n`;
+	const unwrappedText = text.substring(columnWidth);
+	return `${wrappedText}${unwrappedText}`;
 }
