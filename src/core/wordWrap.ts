b1e7b88511f2c524a11f7a3e58f008fdce0ba2ca
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-04-18 15:56:42 -0500

    green(wordWrap): should makes every single line of text fit column width

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,7 +1,15 @@
 export function wordWrap(text: string, columnWidth: number): string {
 	if (text.length <= columnWidth) return text;
 
-	const wrappedText = text.substring(0, columnWidth).concat('\n');
-	const unwrappedText = text.substring(columnWidth);
-	return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
+	let wrappedText = text.substring(0, columnWidth).concat('\n');
+	let unwrappedText = text.substring(columnWidth);
+	let acumulatedText = wrappedText;
+
+	while (unwrappedText.length > columnWidth) {
+		wrappedText = unwrappedText.substring(0, columnWidth).concat('\n');
+		unwrappedText = unwrappedText.substring(columnWidth);
+		acumulatedText += wrappedText;
+	}
+
+	return `${acumulatedText}${unwrappedText}`;
 }
--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -4,6 +4,7 @@ describe('The Word Wrap', () => {
 	test('should makes every single line of text fit column width', () => {
 		expect(wordWrap('hello', 5)).toBe('hello');
 		expect(wordWrap('longword', 4)).toBe('long\nword');
+		expect(wordWrap('longword', 3)).toBe('lon\ngwo\nrd');
 		expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
 	});
 });
