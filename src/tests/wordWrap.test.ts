c29940e0c58ba192e0695d753c6ea0e002bde4b8
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2023-05-09 11:47:58 -0500

    refactor(wordWrap): Regroup tests

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,15 +1,24 @@
 import { wordWrap } from '../core/wordWrap';
 
 describe('The Word Wrap', () => {
-	test('should makes every single line of text fit column width', () => {
-		expect(wordWrap('hello', 5)).toBe('hello');
-		expect(wordWrap('longword', 4)).toBe('long\nword');
-		expect(wordWrap('longword', 3)).toBe('lon\ngwo\nrd');
-		expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
-		expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
-		expect(wordWrap(' abcd', 4)).toBe('\nabcd');
+	test('should empty text does not need to be wrapped', () => {
+		expect(wordWrap('', 5)).toBe('');
 		expect(wordWrap(null, 5)).toBe('');
 		expect(wordWrap(undefined, 5)).toBe('');
+	});
+	test('should small text does not need to be wrapped', () => {
+		expect(wordWrap('hello', 5)).toBe('hello');
+	});
+	test('should words are wrapped when do not fit the column width', () => {
+		expect(wordWrap('longword', 4)).toBe('long\nword');
+		expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
+	});
+	test('should spaces are preferred for wrapping', () => {
+		expect(wordWrap('longword', 3)).toBe('lon\ngwo\nrd');
+		expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
+		expect(wordWrap(' abcd', 4)).toBe('\nabcd');
+	});
+	test('should does not allow for negative column with', () => {
 		expect(() => wordWrap('hello', -5)).toThrow('Negative column width is not allowed');
 	});
 });
